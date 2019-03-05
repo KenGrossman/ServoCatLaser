7b258ae20cd8233ff2616b07dda7bd2ef5bc7f94
Kenny Grossman <mrkengrossman@gmail.com>
2019-03-04 22:38:40 -0500

    Add files via upload
    Created test case to verify range of servo motion and verify that laser works

--- ServoController.py
+++ ServoController.py
@@ -4,11 +4,12 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
 MIN_DUTY = 1
-MAX_DUTY = 12
+MAX_DUTY = 11
 CENTER = MIN_DUTY + (MAX_DUTY-MIN_DUTY) / 2
-ONE_DEGREE = MAX_DUTY / 180
+ONE_DEGREE = MAX_DUTY / 90
 
 #Setup pin 7 for Pulse width modulation of 50hz
 xAxis= GPIO.PWM(7,50)
@@ -18,59 +19,60 @@ yAxis= GPIO.PWM(5,50)
 #Set starting location to 
 xAxis.start(CENTER)
 yAxis.start(CENTER)
+GPIO.output(12, 1)
 
-try:
+def test1():
     cycle = MIN_DUTY
+    print("Top Right to Bottom Left - Start")
     while cycle <= MAX_DUTY:
         xAxis.ChangeDutyCycle(cycle)
         yAxis.ChangeDutyCycle(cycle)
-        print ("DEGREES", cycle)
+        print ("X", cycle, "Y", cycle)
         cycle = cycle + ONE_DEGREE
         time.sleep(.1)
-        if (cycle >= 12):
-            cycle = 0
-        
-#        p.ChangeDutyCycle(cycle)
-#        print ("DEGREES", cycle)
-#        cycle = cycle + ONE_DEGREE
-#        time.sleep(.1)
-                #90 degrees
-#        p.ChangeDutyCycle(11)
-#        time.sleep(1)
-#        print("Right 90 degrees")
-        
-                #-90 degrees
-#        p.ChangeDutyCycle(2)
-#        time.sleep(1)
-#        print("x1 degrees")
-#        
-#        
-#        #-90 degrees
-#        p.ChangeDutyCycle(2.5)
-#        time.sleep(1)
-#        print("0 degrees")
-#        
-#                #-90 degrees
-#        p.ChangeDutyCycle(3)
-#        time.sleep(1)
-#        print("x degrees")
-#        
-#        #-45 degrees
-#        p.ChangeDutyCycle(5.75)
-#        time.sleep(1)
-#        print("Left 45 degrees")
-#        
-#        #0 degrees
-#        p.ChangeDutyCycle(7.5)
-#        time.sleep(1)
-#        print("Left 90 degrees")
-#        
-#        #45 degrees
-#        p.ChangeDutyCycle(10)
-#        time.sleep(1)
-#        print("Left X degrees")  
 
+def test2():
+    cycle = MAX_DUTY
+    print("Bottom Left to Top Right - Start")
+    while cycle >= MIN_DUTY:
+        xAxis.ChangeDutyCycle(cycle)
+        yAxis.ChangeDutyCycle(cycle)
+        print ("X", cycle, "Y", cycle)
+        cycle = cycle - ONE_DEGREE
+        time.sleep(.1)
         
+def test3():
+    x = MAX_DUTY
+    y = MIN_DUTY
+    print("Top Left to Bottom Right - Start")
+    while x >= MIN_DUTY:
+        xAxis.ChangeDutyCycle(x)
+        yAxis.ChangeDutyCycle(y)
+        print ("X", x, "Y", y)
+        x = x - ONE_DEGREE
+        y = y + ONE_DEGREE
+        time.sleep(.1)
+        
+def test4():
+    x = MIN_DUTY
+    y = MAX_DUTY
+    print("Bottom Right to Top Left - Start")
+    while y >= MIN_DUTY:
+        xAxis.ChangeDutyCycle(x)
+        yAxis.ChangeDutyCycle(y)
+        print ("X", x, "Y", y)
+        x = x + ONE_DEGREE
+        y = y - ONE_DEGREE
+        time.sleep(.1)
+
+#Cycle from top right to bottom left
+try:
+    test1()
+    test2()
+    test3()
+    test4()
+    GPIO.cleanup()
+    
 except KeyboardInterrupt:
     p.stop()
 
